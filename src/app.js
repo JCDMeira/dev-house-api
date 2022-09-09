e1e7a38e71f05cd8d5bdab6ecac7da7d014ae96f
Jean Meira <jean.meira10@hotmail.com>
2022-09-08 21:25:25 -0300

    feat: connect mongoose

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import routes from './routes';
 
 class App {
   constructor() {
     this.server = express();
+
+    mongoose.connect(
+      'mongodb+srv://progApi:progApi@cluster0.jhwjo5u.mongodb.net/devHouse?retryWrites=true&w=majority',
+      { useNewUrlParser: true, useUnifiedTopology: true },
+    );
 
     this.middlewares();
     this.routes();
