eba00e7c815dff07b1deaf1bd1c6cd8cf81c566a
Jean Meira <jean.meira10@hotmail.com>
2022-09-10 21:14:12 -0300

    feat: add House Controller

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from './config/upload';
 
 import SessionControler from './controllers/SessionController';
+import HouseController from './controllers/HouseController';
 
 const routes = new Router();
+const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionControler.store);
+
+routes.post('/houses', upload.single('thumbnail'), HouseController.store);
 
 export default routes;
