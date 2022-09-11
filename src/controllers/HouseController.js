813e294901f2bd4486867e0493b0ad4d94198432
Jean Meira <jean.meira10@hotmail.com>
2022-09-11 20:19:45 -0300

    feat: implements store method in House

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,8 +1,21 @@
+import House from '../models/House';
 class HouseController {
   async store(req, res) {
-    console.log(req.body);
-    console.log(req.file);
-    return res.json({ ok: true });
+    const { filename } = req.file;
+    const { description, price, location, status } = req.body;
+    const { user_id } = req.headers;
+    const fileNameWithoutSpaces = filename.replace(' ', '');
+
+    const house = await House.create({
+      user: user_id,
+      thumbnail: fileNameWithoutSpaces,
+      description,
+      price,
+      location,
+      status,
+    });
+
+    return res.json(house);
   }
 }
 
