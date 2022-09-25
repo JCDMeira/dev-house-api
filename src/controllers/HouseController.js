665c0c203522ea2740da5ac80ca995c7bb034682
Jean Meira <jean.meira10@hotmail.com>
2022-09-25 10:48:59 -0300

    feat: update House validations

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,5 +1,6 @@
 import House from '../models/House';
 import User from '../models/User';
+import * as Yup from 'yup';
 class HouseController {
   async index(req, res) {
     const { status } = req.query;
@@ -9,21 +10,36 @@ class HouseController {
     return res.json(houses);
   }
   async store(req, res) {
-    const { filename } = req.file;
-    const { description, price, location, status } = req.body;
-    const { user_id } = req.headers;
-    const fileNameWithoutSpaces = filename.replace(' ', '');
+    try {
+      const { filename } = req.file;
+      const { description, price, location, status } = req.body;
+      const { user_id } = req.headers;
+      const fileNameWithoutSpaces = filename.replace(' ', '');
 
-    const house = await House.create({
-      user: user_id,
-      thumbnail: fileNameWithoutSpaces,
-      description,
-      price,
-      location,
-      status,
-    });
+      const schema = Yup.object().shape({
+        description: Yup.string().required(),
+        price: Yup.number().required(),
+        location: Yup.string().required(),
+        status: Yup.boolean().required(),
+      });
 
-    return res.json(house);
+      if (!(await schema.isValid(req.body))) {
+        return res.status(400).json({ msg: 'Falha na validação.' });
+      }
+
+      const house = await House.create({
+        user: user_id,
+        thumbnail: fileNameWithoutSpaces,
+        description,
+        price,
+        location,
+        status,
+      });
+
+      return res.json(house);
+    } catch (error) {
+      return res.status(400).json({ msg: `${error}` });
+    }
   }
 
   async update(req, res) {
@@ -33,6 +49,17 @@ class HouseController {
       const { description, price, location, status } = req.body;
       const { user_id } = req.headers;
       const fileNameWithoutSpaces = filename.replace(' ', '');
+
+      const schema = Yup.object().shape({
+        description: Yup.string().required(),
+        price: Yup.number().required(),
+        location: Yup.string().required(),
+        status: Yup.boolean().required(),
+      });
+
+      if (!(await schema.isValid(req.body))) {
+        return res.status(400).json({ msg: 'Falha na validação.' });
+      }
 
       const user = await User.findById(user_id);
       const houses = await House.findById(house_id);
